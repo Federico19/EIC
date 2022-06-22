f4e414be99b4b5de959fd28a43ed9fb2a16f8395
Federico Ocampo <30559203+Federico19@users.noreply.github.com>
2022-06-22 11:29:14 -0300

    Add .eslinter.json

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 //import '@testing-library/jest-dom/extend-expect';
-import App from './App';
+import App from "./App";
+import React from "react";
 
 // test('renders content',()=>{
 //   const utils = render(<App />);
@@ -9,10 +10,10 @@ import App from './App';
 //   //expect(component.container).toHaveTextContent('2 y 2 son 4!');
 // });
 
-test('2 y 2 son 4!', () => {
-  render(<App />);
-  const linkElement = screen.getByText('2 y 2 son 4!');
-  expect(linkElement).toBeInTheDocument();
+test("2 y 2 son 4!", () => {
+	render(<App />);
+	const linkElement = screen.getByText("2 y 2 son 4!");
+	expect(linkElement).toBeInTheDocument();
 }); 
 
 
