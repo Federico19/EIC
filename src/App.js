b0a2dc48cb20c89ed4dc9e749dd5ef181befd941
Federico Ocampo <30559203+Federico19@users.noreply.github.com>
2022-06-21 21:29:27 -0300

    Error introducido en App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>2 y 2 son {Sum(2,2)}!</h2>
+        <h2>2 y 2 son {Sum(2,3)}!</h2>
         {/* <img src="https://qph.fs.quoracdn.net/main-qimg-bd2bccb58dc44e35ab3befe5508e40c5-pjlq"
               alt= "El gallo Claudio"></img> */}
       </header>
