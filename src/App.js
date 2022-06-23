b9526aa1e158448892b06c07848438a4a58d75ec
Federico Ocampo <30559203+Federico19@users.noreply.github.com>
2022-06-23 11:41:08 -0300

    Limpiando App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-//import logo from './logo.svg';
 import React from "react";
 import "./App.css";
 import Sum from "./components/Sum.js";
@@ -14,28 +13,5 @@ function App() {
 		</div>
 	);
 }
-
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          2
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
 
 export default App;
